95e8e3af6ad39ed8457d0f29fb6c8d6e99f9c206
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2023-02-27 23:53:28 +0000

    refactor: add aliases

--- node/src/index.test.ts
+++ node/src/index.test.ts
@@ -1,4 +1,4 @@
-import { maxSubArray } from "./index";
+import { maxSubArray } from "@/index";
 
 it("sum of max sub array is 6", () => {
   expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
